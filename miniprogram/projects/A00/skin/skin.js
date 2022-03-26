7e84e8f41350354f7a3165a638b355466667abcf
xb <xb@foxmail.com>
2022-03-26 20:02:41 +0800

    CR

---
+++ miniprogram/projects/A00/skin/skin.js
@@ -0,0 +1,43 @@
+module.exports = {
+	PID: 'A00', // 羽毛球
+
+	NAV_COLOR: '#ffffff',
+	NAV_BG: '#504A84',
+
+	MEET_NAME: '预约', 
+ 
+	MENU_ITEM: ['首页', '预约日历', '我的'], // 第1,4,5菜单
+
+	NEWS_CATE: '1=本馆动态|leftpic,2=运动常识|rightpic',
+	MEET_TYPE: '1=场地预约|leftbig2',
+
+	DEFAULT_FORMS: [{
+			type: 'line',
+			title: '姓名',
+			desc: '请填写您的姓名',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		},
+		{
+			type: 'line',
+			title: '手机',
+			desc: '请填写您的手机号码',
+			must: true,
+			len: 50,
+			onlySet: {
+				mode: 'all',
+				cnt: -1
+			},
+			selectOptions: ['', ''],
+			mobileTruth: true,
+			checkBoxLimit: 2,
+		}
+	]
+}
